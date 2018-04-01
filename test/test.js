f7c90839d3a739c26fd47adbd4d51ffc48d60568
Krakadet <krakadet@gmail.com>
2018-04-01 16:13:39 +0300

    Add a test.html and test which use the chai and mocha framework.

---
+++ test/test.js
@@ -0,0 +1,46 @@
+
+/*describe("missingNumber", function() {
+
+    it("возводит в n-ю степень", function() {
+        assert.equal(pow(2, 3), 8);
+    });
+
+});*/
+describe("Permutation check", function() {
+    it("check is a current array permutation and corresponds to the condition as sequence containing each element from 1 to N once", function() {
+        assert.equal(permutation([4, 1, 3]), 0);
+        assert.equal(permutation([4, 1, 3, 4]), 0);
+        assert.equal(permutation([4]), 0);
+        assert.equal(permutation([1]), 1);
+        assert.equal(permutation([1,4,2,3]), 1);
+    });
+
+});
+describe("Permutation missing elem", function () {
+    it("return element is missin at permutation array", function() {
+        assert.equal(permMissingElem([1,4,2,3]), 5);
+        assert.equal(permMissingElem([1,5,2,3]), 4);
+        assert.equal(permMissingElem([1]), 2);
+        assert.equal(permMissingElem([5,4,2,3]), 1);
+        assert.equal(permMissingElem([]), 1);
+    });
+});
+describe("TapeEquilibrium", function() {
+
+    it(" returns the minimal difference that can be achieved.", function() {
+        assert.equal(tapeEquilibrium([3,1,2,4,3]), 1);
+        assert.equal(tapeEquilibrium([-1000,1000]), 2000);
+        assert.equal(tapeEquilibrium([1,1]), 0);
+    });
+
+});
+describe("Frog jump", function() {
+
+    it("return minimal number of jump for move from point A to B. Function take three integer number (A,B,D), where 'D' is a distance a frog jump", function() {
+        assert.equal(frogJump(10,85,30), 3);
+        assert.equal(frogJump(10,10,30), 0);
+        assert.equal(frogJump(10,10000000,30), 333333);
+        assert.equal(frogJump(0,1000,1000), 1);
+    });
+
+});
